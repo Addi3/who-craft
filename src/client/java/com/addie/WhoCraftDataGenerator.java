b782e742dacf08cdd1f11ce4e5bd0f03d00bf64a
Addi3 <addiec787@gmail.com>
2026-01-03 17:06:16 +0000

    init commit

---
+++ src/client/java/com/addie/WhoCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package com.addie;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WhoCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
